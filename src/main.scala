b72625bc1f673f46b1b8feb99d3d1a4ee59742fd
Eli45 <elifurland@gmail.com>
2014-12-29 07:20:52 -0500

    BUILD v.0.0.0.1

--- src/main.scala
+++ src/main.scala
@@ -19,12 +19,28 @@ object main	{
 		} while (retry);
 		println("You chose to play as: " + player.Name);
 	};
+	//Put the below inside the attack country function(to be added later.)
+	def check_untargetable_states(enemy:String):Boolean =	{
+		var valid = false;
+		for (i <- 0 to untargetable_states.length - 1)	{
+			if (untargetable_states(i).Name == enemy)	{ valid = true; }
+		}
+		return valid;
+	};
+	
 	//^ Move these to functions.
 	
 	def main(args: Array[String]) =	{
 		var gm = new gm();
 		pickNation();
-		
-		
+		while (player.Pop > 0)	{
+			gm.exec_player_events();
+			gm.exec_scripted_events();
+			/*
+			 * Choose target.
+			 * Combat between target and player.
+			 */
+			gm.nextTurn();
+		}
 	}
 }
