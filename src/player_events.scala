c85007e9b29e49ed25812a3a8b292110849e7e4d
Eli45 <elifurland@gmail.com>
2014-12-29 07:53:17 -0500

    updates player_events

--- src/player_events.scala
+++ src/player_events.scala
@@ -1,10 +1,16 @@
 object player_events	{
 	import game_manager._, globalVar._;
 	def player_events(game_manager:gm) =	{
-		if (player.Name == "The United States of America")	{}
-		else if (player.Name == "The Soviet Union")	{}
-		else if (player.Name == "France")	{}
-		else if (player.Name == "The United Kingdom")	{}
-		else if (player.Name == "China")	{} 
-	}
+		if (player.Name == "The United States of America")	{ us_events(); }
+		else if (player.Name == "The Soviet Union")			{ sov_events(); }
+		else if (player.Name == "France")					{ fra_events(); }
+		else if (player.Name == "The United Kingdom")		{ bri_events(); }
+		else if (player.Name == "China")					{ chi_events(); } 
+	};
+	//Private may or may not work in this scenario. In the event of any errors check this first.
+	private def us_events() =	{};
+	private def sov_events() =	{};
+	private def fra_events() =	{};
+	private def bri_events() =	{};
+	private def chi_events() =	{}
 }
